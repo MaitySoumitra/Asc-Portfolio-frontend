29df259fb6315d1bc1c8564ce37d8926b92d7d1c
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2025-10-17 19:48:31 +0530

    frontend added

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -8,7 +8,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin/portfolio" element={<Admin />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
